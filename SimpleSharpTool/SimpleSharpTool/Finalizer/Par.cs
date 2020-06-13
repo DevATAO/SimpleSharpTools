67dd978d50c6e1d385d0e6584f9a63a1a9c7ece9
Anwise.Tao <tao.shan@anwiseglobal.com>
2020-06-13 11:08:55 +0800

    添加工具类

---
+++ SimpleSharpTool/SimpleSharpTool/Finalizer/Par.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleSharpTool.Finalizer
+{
+    public class Par:IDisposable
+    {
+        public virtual void Dispose()
+        {
+            //释放资源
+        }
+
+        ~Par()
+        {
+            //释放资源
+        }
+    }
+}
